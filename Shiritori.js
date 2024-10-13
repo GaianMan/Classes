0a83807f68352358f9a9040624d99d213fdccc18
Gayaneh Mandani <gaianbk@gmail.com>
2024-10-13 18:19:54 +0400

    adding latest update of shiritori

--- Shiritori.js
+++ Shiritori.js
@@ -1,60 +1,45 @@
-// This challenge is an English twist on the Japanese word game Shiritori. The basic premise is to follow two rules:
-// First character of next word must match last character of previous word.
-// The word must not have already been said.
-// Below is an example of a Shiritori game:
-// ["word", "dowry", "yodel", "leader", "righteous", "serpent"]; // valid!
-// ["motive", "beach"]; // invalid! - beach should start with "e"
-// ["hive", "eh", "hive"]; // invalid! - "hive" has already been said
 
-
-
-// Methods:
-
-// restart: a method that sets the words array to an empty one [] and sets the game_over boolean to
-// false. It should return "game restarted".
-// myShiritori = new Shiritory();
-// myShiritori.play("apple"); // ["apple"]
-// myShiritori.play("ear"); // ["apple", "ear"]
-// myShiritori.play("rhino"); // ["apple", "ear", "rhino"]
-// myShiritori.play("corn"); // "game over"
-// // Corn does not start with an "o".
-// myShiritori.words; // ["apple", "ear", "rhino"]
-// // Words should be accessible.
-// myShiritori.restart(); // "game restarted"
-// myShiritori.words; // []
-
-// // Words array should be set back to empty.
-// myShiritori.play("hostess"); // ["hostess"]
-// myShiritori.play("stash"); // ["hostess", "stash"]
-// myShiritori.play("hostess"); // "game over"
-
-// Write a Shiritori class that has two instance properties:
-// words: an array of words already said.
-// game_over: a boolean that is true if the game is over.
-class Shiritoni{
-    constructor(words,game_over){
-        this.words=[];
-        this.game_over=false;
+class Shiritoni {
+    constructor(words, game_over) {
+      this.words = [];
+      this.game_over = false;
     }
-}
-
-// play: a method that takes in a word as an argument and checks if it is valid (the word should follow rules
-// #1 and #2 above).
-// If it is valid, it adds the word to the words array, and returns the words array.
-// If it is invalid (eitherrule is broken), itreturns "game over" and sets the game_over boolean to
-// true.
-function hasRules(w){
-    let wordLength=w.length;
-    const wordArr=this.words;
-    for(char of wordArr){
-        if (w[wordLength]===w[0]){}
-
+  
+    play(word) {
+      let wordLenght=this.word;
+      if (this.game_over) {
+        return "game over";
+      }
+  
+      if (this.words.includes(word)) {
+        this.game_over = true;
+        return "game over";
+      }
+  
+      if (this.words.length > 0) {
+        let lastWord = this.words[this.words.length - 1];
+        if (word[0] !== lastWord[lastWord.length - 1]) {
+          this.game_over = true;
+          return "game over";
+        }
+      }
+      this.words.push(word);
+      return this.words;
     }
-}
-Shiritoni.prototype.play=function(wrd){
-    words(){
-        this.wordArr=[];
+  
+    restart() {
+      this.words = [];
+      this.game_over = false;
+      return "game restarted";
     }
-
-
-}
+  }
+  
+  let game = new Shiritoni();
+  
+  console.log(game.play("apple"));
+  console.log(game.play("ear"));
+  console.log(game.play("rhino"));
+  console.log(game.play("corn")); 
+  console.log(game.words);
+  console.log(game.restart());
+  console.log(game.words);
